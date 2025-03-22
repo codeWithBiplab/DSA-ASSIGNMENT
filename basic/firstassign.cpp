83fbaf738bddaf68ae92e14f5a99d98888f2f86f
codeWithBiplab <biplabgoswami023@gmail.com>
2025-03-22 23:10:13 +0530

    first commit

---
+++ basic/firstassign.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+//     // sum of numbers
+//     float a , b;
+//     cout<< " enter your values";
+//     cin>>a>>b;
+//     cout<<"sum of the number is \n"<<a+b<<"\n";
+// //     area of rectangle
+// // 
+// cout<<a*b<<"\n";
+// cout<<a/b<<"\n";
+// cout<<a-b<<"\n";
+
+// simple interest
+// int p ,r ,t ;
+// cin>>p>>r>>t;
+// cout<<(p*r*t)/100<<"\n";
+// char ch ;
+// cin>>ch;
+// cout<<(int)ch<<"\n";
+// int x = 5;
+// cout<<x++;
+int n;
+cin>>n;
+if (n>99 && n<999)
+{
+    cout<<"3 digit number ";
+}
+else {
+    cout<<"not a 3 digit number";
+}
+
+}
